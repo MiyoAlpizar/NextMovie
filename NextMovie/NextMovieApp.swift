383b47307d6e5617fc61c48b6d86a5a936daad36
Miyo Alpízar <miyo.naval@gmail.com>
2020-10-30 17:45:56 -0600

    Initial Commit

---
+++ NextMovie/NextMovieApp.swift
@@ -0,0 +1,17 @@
+//
+//  NextMovieApp.swift
+//  NextMovie
+//
+//  Created by Miyo Alpízar on 30/10/20.
+//
+
+import SwiftUI
+
+@main
+struct NextMovieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
